b44ed0c51a47c1df39e06d210bd4cd4330a2611a
Vitor_Tigre <vitorrtigre@gmail.com>
2025-06-05 18:15:17 -0300

    terminei a logo

--- assets/scripts/mode.js
+++ assets/scripts/mode.js
@@ -1,17 +1,24 @@
 const themeToggleBtn = document.querySelector('header button');
 const themeIcon = themeToggleBtn.querySelector('img');
+const logo = document.getElementById('logo');
 
 const moonIconPath = './assets/images/lua-crescente.png';
 const sunIconPath = './assets/images/sol.png';
+const darkLogoPath = './assets/images/logoclara.png';
+const lightLogoPath = './assets/images/logoescura.png';
+
+
 
 function enableDarkMode() {
   document.documentElement.setAttribute('data-theme', 'dark');
   themeIcon.src = moonIconPath;
+  logo.src = darkLogoPath;
 }
 
 function enableLightMode() {
   document.documentElement.setAttribute('data-theme', 'light');
   themeIcon.src = sunIconPath;
+  logo.src = lightLogoPath;
 }
 
 function toggleTheme() {
@@ -26,3 +33,4 @@ function toggleTheme() {
 themeToggleBtn.addEventListener('click', toggleTheme);
 
 enableDarkMode();
+
